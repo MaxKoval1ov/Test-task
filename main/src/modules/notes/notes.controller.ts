48897b33050cbe87b456ffd06e475198e12c5324
MaxKoval1ov <maxkov112211@gmail.com>
2022-04-23 05:45:18 +0300

    feat:add swagger docs

--- main/src/modules/notes/notes.controller.ts
+++ main/src/modules/notes/notes.controller.ts
@@ -12,19 +12,23 @@ import {
   Put,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { lastValueFrom } from 'rxjs';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { NotesActions } from './notes.actions';
 
+@ApiTags('Заметки')
 @Controller('notes')
 export class NotesController {
   constructor(@Inject('NOTES_SERVICE') private readonly client: ClientProxy) {}
 
+  @ApiOperation({ summary: 'Получить заметку по её id' })
   @Get(':id')
   getById(@Param('id') id: string) {
     return this.client.emit(NotesActions.GET_NOTE, id);
   }
 
+  @ApiOperation({ summary: 'Получить все заметки' })
   @Get()
   getAllNotes() {
     return lastValueFrom(
@@ -32,6 +36,7 @@ export class NotesController {
     );
   }
 
+  @ApiOperation({ summary: 'Добавить заметку' })
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
@@ -39,6 +44,7 @@ export class NotesController {
     return this.client.send(NotesActions.CREATE_NOTE, createNote).toPromise();
   }
 
+  @ApiOperation({ summary: 'Удалить заметку' })
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.client.send(NotesActions.DELETE_NOTE, id).toPromise();
@@ -51,11 +57,13 @@ export class NotesController {
       .toPromise(); //LastValueFrom
   }
 
+  @ApiOperation({ summary: 'Получить все заметки выбранного пользователя' })
   @Get('users/:id')
   getNodesByUserId(@Param('id') id: number) {
     return this.client.send(NotesActions.GET_NOTES_BY_USERID, id).toPromise();
   }
 
+  @ApiOperation({ summary: 'Обновить заметку' })
   @Put()
   update(@Body() updatedNote: CreateNoteDto) {
     return this.client.send(NotesActions.UPDATE_NOTE, updatedNote).toPromise();
--- main/src/modules/users/users.controller.ts
+++ main/src/modules/users/users.controller.ts
@@ -9,29 +9,35 @@ import {
   Put,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { lastValueFrom } from 'rxjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserActions } from './users.actions';
 
+@ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
   constructor(@Inject('USERS_SERVICE') private readonly client: ClientProxy) {}
 
+  @ApiOperation({ summary: 'Создать пользователя' })
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.client.send(UserActions.CREATE_USER, userDto).toPromise();
   }
 
+  @ApiOperation({ summary: 'Удалить пользователя' })
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.client.send(UserActions.DELETE_USER, id).toPromise();
   }
 
+  @ApiOperation({ summary: 'Получить всех пользователей' })
   @Get()
   getAll() {
     return lastValueFrom(this.client.send(UserActions.GET_ALL_USERS, ''));
   }
 
+  @ApiOperation({ summary: 'Обновить пользователя' })
   @Put()
   update(@Body() newUser: CreateUserDto) {
     return this.client.send(UserActions.UPDATE_USER, newUser).toPromise();
